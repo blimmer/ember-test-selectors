418b27ea36f9cc15b0506397ded8530e21cf4df9
pangratz <cmueller.418@gmail.com>
2016-03-02 12:17:13 +0100

    Add the plugin in the setupPreprocessorRegistry

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var StringTestSelectorsTransform = require('./strip-test-selectors');
 module.exports = {
   name: 'test-selectors',
 
-  included: function() {
-    if (this.app.env !== 'development' && this.app.env !== 'test') {
-      this.app.registry.add('htmlbars-ast-plugin', {
+  setupPreprocessorRegistry: function(type, registry) {
+    if (registry.app.env !== 'development' && registry.app.env !== 'test') {
+      registry.add('htmlbars-ast-plugin', {
         name: 'strip-test-selectors',
         plugin: StringTestSelectorsTransform
       });
